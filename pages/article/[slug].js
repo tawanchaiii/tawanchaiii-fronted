907f650d1290ae0539eb0cb5f1f9a515e71d1a98
tawanchaiii <tawanchai_champ@hotmail.com>
2021-11-13 14:02:47 +0700

    prettier 2

--- pages/article/[slug].js
+++ pages/article/[slug].js
@@ -1,5 +1,5 @@
 import ReactMarkdown from "react-markdown"
-import React from 'react';
+import React from "react"
 import Moment from "react-moment"
 import { fetchAPI } from "../../lib/api"
 import Layout from "../../components/layout"
